cec925fb751de994327a27d73dda65ac62d0304a
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2024-09-30 19:09:25 -0700

    Refactor to add future value calculation function
    Extracted future value calculation into a separate function. Added constant to account for inflation rate and included its calculation in the output. This improves code readability and reusability.

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,10 +6,18 @@ import (
 )
 
 func main() {
+	const inflationRate = 2.5
 	investmentAmount := 1000.0
 	years := 10.0
 	expectedReturnRate := 5.5
 
-	futureValue := investmentAmount * math.Pow(1+(expectedReturnRate/100), years)
-	fmt.Println(futureValue)
+	fv := futureValue(investmentAmount, years, expectedReturnRate)
+	inflationAdjustedFV := futureValue(investmentAmount, years, inflationRate)
+
+	fmt.Println(fv)
+	fmt.Println(inflationAdjustedFV)
+}
+
+func futureValue(investmentAmount float64, years float64, rate float64) float64 {
+	return investmentAmount * math.Pow(1+(rate/100), years)
 }
