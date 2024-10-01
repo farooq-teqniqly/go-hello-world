7f619979b1a104db756da567c2a408e6055f6694
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2024-10-01 11:14:58 -0700

    Prompt for user input in the investment calculator.
    Modified the investment calculator to take user input for the investment amount, number of years, and expected rate of return. Additionally, improved output clarity by adding labels to the printed future value and inflation-adjusted future value. This enhances user interaction and makes the tool more versatile.

--- investment_calculator.go
+++ investment_calculator.go
@@ -7,15 +7,36 @@ import (
 
 func main() {
 	const inflationRate = 2.5
-	investmentAmount := 1000.0
-	years := 10.0
-	expectedReturnRate := 5.5
+	var investmentAmount float64
+	var years float64
+	var expectedReturnRate float64
+
+	fmt.Print("Enter investment amount: ")
+	_, err := fmt.Scan(&investmentAmount)
+
+	if err != nil {
+		return
+	}
+
+	fmt.Print("Enter the number of years: ")
+	_, err = fmt.Scan(&years)
+
+	if err != nil {
+		return
+	}
+
+	fmt.Print("Enter the expected rate of return: ")
+	_, err = fmt.Scan(&expectedReturnRate)
+
+	if err != nil {
+		return
+	}
 
 	fv := futureValue(investmentAmount, years, expectedReturnRate)
 	inflationAdjustedFV := futureValue(investmentAmount, years, inflationRate)
 
-	fmt.Println(fv)
-	fmt.Println(inflationAdjustedFV)
+	fmt.Println("Future value: ", fv)
+	fmt.Println("Inflation adjusted future value (", inflationRate, " inflation rate)", inflationAdjustedFV)
 }
 
 func futureValue(investmentAmount float64, years float64, rate float64) float64 {
